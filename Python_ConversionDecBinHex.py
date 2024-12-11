efa736a47c99127d47cec06711fac80f5747f63e
MDulche <mdulche.btsciel24@eleve-irup.com>
2024-12-11 20:49:22 +0100

    commit

--- Python_ConversionDecBinHex.py
+++ Python_ConversionDecBinHex.py
@@ -1,9 +1,19 @@
-def conversion_dec_bin_hex(number):
+def conversion_dec(number):
     dec = int(number)
     binary = bin(dec)[2:]  # Supprime le préfixe "0b"
     hexa = hex(dec)[2:].upper()  # Supprime le préfixe "0x" et met en majuscules
     return f"Décimal: {dec}, Binaire: {binary}, Hexadécimal: {hexa}"
 
+def conversion_hex(number):
+    dec = int(number, 16)  # Convertir la chaîne hexadécimale en nombre décimal
+    binary = bin(dec)[2:]  # Convertir le nombre décimal en binaire et supprimer le préfixe "0b"
+    return f"Hexadécimal: {number}, Décimal: {dec}, Binaire: {binary}"
+
+def conversion_bin(number):
+    dec = int(number, 2)  # Convertir la chaîne binaire en nombre décimal
+    hexa = hex(dec)[2:].upper()  # Convertir le nombre décimal en hexadécimal et supprimer le préfixe "0x" et mettre en majuscules
+    return f"Binaire: {number}, Décimal: {dec}, Hexadécimal: {hexa}"
+
 # Exemple de test
 number = input("Entrez un nombre décimal: ")
-print(conversion_dec_bin_hex(number))
+print(conversion_dec(number))
